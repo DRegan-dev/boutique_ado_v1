0c98068a4fb1372b4de4f01a43a35ab2b62f756e
Damien <damienreganmusic@gmail.com>
2025-03-22 13:55:08 +0000

    updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def bag_contents(request):
 
     bag_items = []
     total = Decimal(0)  # Ensure total is a Decimal
     product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity* product.price
+        product_count += quantity
+        bag_items.append ({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product
+
+        })
 
     free_delivery_threshold = Decimal(settings.FREE_DELIVERY_THRESHOLD)
     standard_delivery_percentage = Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
