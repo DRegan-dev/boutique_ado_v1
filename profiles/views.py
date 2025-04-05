7d5c13e5335f1967e79d650e60816204b5156a3b
Damien <damienreganmusic@gmail.com>
2025-04-05 16:00:12 +0100

    Product Admin - finieshed add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-from checkout.models import Order
 
-import .models import UserProfile
+from .models import UserProfile
 from .forms import UserProfileForm
 
-# Create your views here.
+from checkout.models import Order
+
 def profile(request):
-    """ Display the user's profile """
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -15,19 +15,21 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance='profile')
-    orders - profile.orders.all()
-    
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
-        'form': form
-        'orders': orders
+        'form': form,
+        'orders': orders,
         'on_profile_page': True
     }
 
     return render(request, template, context)
+
 
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
@@ -42,6 +44,5 @@ def order_history(request, order_number):
         'order': order,
         'from_profile': True,
     }
-
 
     return render(request, template, context)
